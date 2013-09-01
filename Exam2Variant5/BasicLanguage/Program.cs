98a7adcc9da4ba81bbbbb87d4745d7c20a24b263
Dr4g0 <d_d0003@yahoo.com>
2013-09-01 22:13:24 +0300

    ExaminePart2Variant5

---
+++ Exam2Variant5/BasicLanguage/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLanguage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder inputText = new StringBuilder();
+            while (true)
+            {
+                string currentLine = Console.ReadLine();
+                inputText.Append(currentLine);
+                if (currentLine.Contains("EXIT;"))
+                {
+                    break;
+                }
+            }
+            //if (inputText[inputText.Length-1] != '\n')
+            //{
+            //    inputText.Append("\n");
+            //}
+            string[] commands = inputText.ToString().Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            for (long i = 0; i < commands.Length; i++)
+            {
+                commands[i] = commands[i].Trim();
+            }
+            for (long i = 0; i < commands.Length; i++)
+            {
+                StartExecution(commands[i]);
+            }
+            Console.Write('\n');
+        }
+
+        private static void StartExecution(string commandLine)
+        {
+            if (commandLine==String.Empty)
+            {
+                return;
+            }
+            else if (commandLine[0] == 'E')
+            {
+                return;
+            }
+            else if (commandLine[0] == 'F')
+            {
+                long loops = 0;
+                int indexOfFirstLeftBRacket = commandLine.IndexOf('(');
+                int indexOfFirstRightBracket = commandLine.IndexOf(')');
+                int indexOfComma = commandLine.Substring(0,indexOfFirstRightBracket).IndexOf(',');
+                if (indexOfComma<0)
+                {
+                    loops = long.Parse(commandLine.Substring(indexOfFirstLeftBRacket+1, indexOfFirstRightBracket - indexOfFirstLeftBRacket-1));
+                }
+                else
+                {
+                    int firstNumber = int.Parse(commandLine.Substring(indexOfFirstLeftBRacket + 1, indexOfComma - indexOfFirstLeftBRacket-1));
+                    int secondNumber = int.Parse(commandLine.Substring(indexOfComma + 1, indexOfFirstRightBracket-indexOfComma-1));
+                    loops = secondNumber - firstNumber + 1;
+                }
+                commandLine = commandLine.Substring(indexOfFirstRightBracket + 1).Trim();
+                for (long i = 0; i < loops; i++)
+                {
+                    StartExecution(commandLine);
+                }
+            }
+            else if (commandLine[0]=='P')
+            {
+                int indexOfFirstLeftBRacket = commandLine.IndexOf('(');
+                int indexOfFirstRightBracket = commandLine.IndexOf(')');
+                string printText = commandLine.Substring(indexOfFirstLeftBRacket + 1, indexOfFirstRightBracket - indexOfFirstLeftBRacket - 1);
+                Console.Write(printText);
+            }
+            else
+            {
+                commandLine = commandLine.Remove(0, 1);
+            }
+        }
+    }
+}
---
+++ Exam2Variant5/MessagesInABottle/MessagesInABottle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagesInABottle
+{
+    class MessagesInABottle
+    {
+        static int counter=0;
+        static StringBuilder finishText = new StringBuilder();
+
+        static void Main()
+        {
+            string secretMessage = Console.ReadLine();
+            string cipher = Console.ReadLine();
+            string[] letters = new string[26];
+            int index = 0;
+            for (int i = 0; i < cipher.Length; i++)
+            {
+                StringBuilder currentLetter = new StringBuilder();
+                if (Char.IsLetter(cipher[i]))
+                {
+                    currentLetter.Append(cipher[i]);
+                    i++;
+                    while (i<cipher.Length&&Char.IsDigit(cipher[i]))
+                    {
+                        currentLetter.Append(cipher[i]);
+                        i++;
+                    }
+                    i--;
+                }
+                letters[index] = currentLetter.ToString();
+                index++;
+            }
+            Array.Sort(letters);
+            string currentLine = String.Empty;
+            Decoder(secretMessage, letters, currentLine);
+            Console.WriteLine(counter);
+            Console.WriteLine(finishText);
+        }
+
+        private static void Decoder(string secretMessage, string[] letters, string currentLine)
+        {
+            if (secretMessage.Length == 0)
+            {
+                counter++;
+                SaveCurrentLine(currentLine);
+                //currentLine.Clear();
+                //return;
+            }
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (letters[i] != null)
+                {
+                    if (secretMessage.StartsWith(letters[i].Substring(1)))
+                    {
+                        Decoder(secretMessage.Substring(letters[i].Substring(1).Length), letters, currentLine+letters[i][0]);
+                    }
+                }
+            }
+        }
+
+        private static void SaveCurrentLine(string currentLine)
+        {
+            finishText.AppendLine(currentLine);
+        }
+    }
+}
