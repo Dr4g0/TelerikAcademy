24e73951f19cdfaed6ee3cd98211b5bca4f46380
Dr4g0 <d_d0003@yahoo.com>
2013-10-28 18:35:01 +0200

    homework

---
+++ Homeworks/OOPFundPrinciplesPartII_Homework/InvalidRangeException/InvalidRangeException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvalidRangeException
+{
+    class InvalidRangeException<T>
+        : ApplicationException
+    {
+        private readonly T startValue;
+        private readonly T endValue;
+
+        public T EndValue
+        {
+            get { return endValue; }
+        }
+
+        public T StartValue
+        {
+            get { return startValue; }
+        }
+
+        public InvalidRangeException(string msg, T start, T end)
+            : this(msg, null, start, end)
+        { }
+
+        public InvalidRangeException(T start, T end)
+            : this(null, start, end)
+        { }
+
+        public InvalidRangeException(string msg, Exception innerEx, T start, T end)
+            : base(msg, innerEx)
+        {
+            this.startValue = start;
+            this.endValue = end;
+        }
+    }
+}
---
+++ Homeworks/OOPFundPrinciplesPartII_Homework/InvalidRangeException/TestMyException.cs
@@ -0,0 +1,78 @@
+﻿/*Define a class InvalidRangeException<T> that holds information about an error condition related to 
+ * invalid range. It should hold error message and a range definition [start … end].
+ * Write a sample application that demonstrates the InvalidRangeException<int> and 
+ * InvalidRangeException<DateTime> by entering numbers in the range [1..100] and dates 
+ * in the range [1.1.1980 … 31.12.2013].
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvalidRangeException
+{
+    class TestMyException
+    {
+        static void TestRangeValues<T>(T userData, T start, T end)
+            where T : IComparable
+        {
+            if ((dynamic)userData<start||(dynamic)userData>end)
+            {
+                throw new InvalidRangeException<T>("The value is out of the range", start, end);
+            }
+        }
+
+        static void Main()
+        {
+            int startRange = 1;
+            int endRange = 100;
+            int userNumber;
+            string inputNumber;
+
+            do
+            {
+            Console.WriteLine("Input a number in the range [{0}...{1}]",startRange,endRange);
+               inputNumber=Console.ReadLine();
+            }
+            while(!Int32.TryParse(inputNumber,out userNumber));
+            
+            try
+            {
+                TestRangeValues(userNumber, startRange, endRange);
+                Console.WriteLine("Your number {0} is in the range",userNumber);
+            }
+            catch (InvalidRangeException<int> ire)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("{0} [{1}...{2}]", ire.Message, ire.StartValue, ire.EndValue);
+                Console.ResetColor();
+            }
+
+            DateTime startDate = new DateTime(1980, 01, 01);
+            DateTime endDate = new DateTime(2013, 12, 31);
+            DateTime userDate = new DateTime();
+            string inputData;
+
+            do
+            {
+                Console.WriteLine("Input a date between the range [{0:dd.MM.yyyy}...{1:dd.MM.yyyy}]",startDate,endDate);
+                inputData=Console.ReadLine();
+            }
+            while(!DateTime.TryParse(inputData,out userDate));
+
+            try
+            {
+                TestRangeValues(userDate, startDate, endDate);
+                Console.WriteLine("Your date {0:dd.MM.yyyy} is in the range",userDate);
+            }
+            catch (InvalidRangeException<DateTime> ire)
+            {
+               Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("{0} [{1:dd.MM.yyyy}...{2:dd.MM.yyyy}]",ire.Message,ire.StartValue,ire.EndValue);
+                Console.ResetColor();
+            }
+        }
+    }
+}
